0808b993c3e4dbc17bd98bf931167cb5038805c9
Nathan Bosman <nate.bosman@gmail.com>
2022-12-11 00:16:46 -0700

    CSS updates

---
+++ WebPortfolioMVC/Models/ProjectModel.cs
@@ -0,0 +1,17 @@
+﻿namespace WebPortfolioMVC.Models
+{
+    public class ProjectModel
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Url { get; set; }
+
+        public string ThumbnailUrl { get; set; }
+
+        public string DocumentationUrl { get; set; }
+
+        public string GithubUrl { get; set; }
+    }
+}
