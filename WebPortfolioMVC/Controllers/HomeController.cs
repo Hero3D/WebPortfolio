f9281370a2623882d3b9aa733ceba1c401eb534a
Nathan Bosman <nate.bosman@gmail.com>
2022-12-18 15:33:43 -0700

    Contact Message System working

--- WebPortfolioMVC/Controllers/HomeController.cs
+++ WebPortfolioMVC/Controllers/HomeController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WebPortfolioMVC.Models;
@@ -31,9 +33,39 @@ namespace WebPortfolioMVC.Controllers
         }
 
         [HttpPost]
-        public IActionResult Message()
+        public JsonResult Message(MessageModel message)
         {
-            return PartialView("_Contact");
+            if (!ModelState.IsValid) return Json("Invalid Entry");
+
+            try
+            {
+                using (MailMessage mail = new MailMessage())
+                {
+                    mail.From = new MailAddress(message.Email);
+                    mail.To.Add("Nathan.Bosman.Software@gmail.com");
+                    mail.Subject = $"{message.Name} sent a message from your Web Porfolio!";
+                    mail.Body = $@"
+                    <h2>{message.Subject}</h2>
+                    <p> {message.Message} </p>
+                    <p> reply: {message.Email}</p>
+                     ";
+                    mail.IsBodyHtml = true;
+
+                    using (SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587))
+                    {
+                        smtp.Credentials = new NetworkCredential("Nathan.Bosman.Software@gmail.com", "qszdadoxiafdyvjp");
+                        smtp.EnableSsl = true;
+                        smtp.Send(mail);
+                    }
+                }
+
+                return Json("Message Sent!");
+            }
+
+            catch(Exception ex)
+            {
+                return Json("Error: " + ex);
+            }
         }
 
         public IActionResult Home()
---
+++ WebPortfolioMVC/Models/MessageModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace WebPortfolioMVC.Models
+{
+    public class MessageModel
+    {
+        [Required(ErrorMessage =("Email is required"))]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = ("Name is required"))]
+        [StringLength(maximumLength: 50, MinimumLength = 1, ErrorMessage = "Name must be between 1 and 50 characters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Subject is required")]
+        [StringLength(maximumLength: 100, MinimumLength = 1, ErrorMessage = "Subject must be between 1 and 100 characters")]
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "Message is required")]
+        [StringLength(maximumLength:1000, MinimumLength = 1, ErrorMessage = "Message must be between 1 and 1000 characters")]
+        public string Message { get; set; }
+    }
+}
